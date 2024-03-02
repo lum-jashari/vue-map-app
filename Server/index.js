26e4f1f659c46037a18fe067efcdf225f1240f86
lum-jashari <lumijashari@hotmail.com>
2024-03-02 13:50:37 +0100

    mapped request params using node URL module

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-const url = require("url");
+const URL = require("url");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -16,9 +16,14 @@ app.use(cors());
 app.get("/api/search/:query", async (req, res) => {
   try {
     // add api key and query strings
+    const params = new URLSearchParams({
+      access_token: process.env.API_KEY,
+      ...URL.parse(req.url, true).query,
+    });
+    console.log(params);
     const query = req.params.query;
     const results = await axios(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`
     );
     const data = results.data;
     res.status(200).json(data);
