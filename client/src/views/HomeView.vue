985e1f558c1e3cef32a0b90b8fa0056c68a221bf
lum-jashari <lumijashari@hotmail.com>
2024-03-03 15:47:43 +0100

    configured .env file and fixed map UI

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,6 +1,36 @@
-<script setup></script>
+<script setup>
+import leaflet from "leaflet";
+import { onMounted, ref } from "vue";
+
+let map;
+onMounted(() => {
+  // init map
+  map = leaflet.map("map").setView([28.538336, -81.379234], 10);
+  // add tile layer
+  leaflet
+    .tileLayer(
+      `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${
+        import.meta.env.VITE_API_KEY
+      }`,
+      {
+        attribution: '&copy; <a href="http://www.mapbox.com">OpenStreetMap</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: import.meta.env.VITE_API_KEY,
+      }
+    )
+    .addTo(map);
+});
+
+const coords = ref(null);
+const fetchCoord = ref(null);
+</script>
 <template>
-  <h1 class="text-xl text-red-500">Testing Tailwind.css</h1>
+  <div class="h-screen relative">
+    <div id="map" class="h-full z-[1]"></div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
